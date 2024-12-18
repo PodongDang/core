58300bce4ef4eb77021c420ad3f3410539c02b05
Local_Hwang <amongzamong@gmail.com>
2024-12-18 14:41:49 +0900

    feat : Add appconfig for IoC, DI, container

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -1,15 +1,25 @@
 package com.example.core.order;
 
 import com.example.core.discount.DiscountPolicy;
-import com.example.core.discount.FixDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
-import com.example.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
+    //DIP, OCP를 위반 (interface 뿐만 아니라 구체 클래스에도 의존함)
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //인터페이스에만 의존하도록 설계를 변경
+//    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
